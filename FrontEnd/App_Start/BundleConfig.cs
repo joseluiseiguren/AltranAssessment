d87ea9578ee6ca39d91998ceb3aa9af9a2dc4fca
joseluiseiguren <joseluiseiguren@gmail.com>
2017-12-30 19:13:22 -0300

    avancs fron end

--- FrontEnd/App_Start/BundleConfig.cs
+++ FrontEnd/App_Start/BundleConfig.cs
@@ -25,7 +25,9 @@ namespace FrontEnd
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+                      "~/Content/site.css",
+                      "~/Content/loading.css",
+                      "~/Content/font-awesome.min.css"));
         }
     }
 }
